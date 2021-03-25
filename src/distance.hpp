fe3257b67ab2e1d060bf46d8ca3873b66934a10a
Sohil Zidan <zidansohil@gmail.com>
2021-03-26 00:08:21 +0100

    redefine distances jaccard & euclidean

--- src/distance.hpp
+++ src/distance.hpp
@@ -2,11 +2,41 @@
 
 namespace ModelFitting
 {
-    using Vecd = Eigen::VectorXd;
-    using Vec3d = Eigen::Vector3d;
-    using MatXd = Eigen::MatrixXd;
-    using VecXb = Eigen::Matrix<bool, Eigen::Dynamic, 1>;
+    template<typename T>
+    T euclidean_distance(cv::Mat_<T>&, cv::Mat_<T>&, const int&);
+    template<typename T>
+    T jaccard_distance(cv::Mat_<T>&, cv::Mat_<T>&, const int&);
 
-    double euclidean_distance(const Vecd&, const Vecd&);
-    double jaccard_distance(const Vecd&, const Vecd&);
+    template<typename T>
+    T euclidean_distance(cv::Mat_<T> &point_a, cv::Mat_<T> &point_b, const int &_dim_num)
+    {
+        const T * point_a_ptr = reinterpret_cast<T *>(point_a.data);
+        const T * point_b_ptr = reinterpret_cast<T *>(point_b.data);
+
+        T difference, distance = 0;
+        for (size_t dim = 0; dim < _dim_num; ++dim)
+        {
+            difference = *(point_a_ptr++) - *(point_b_ptr++);
+            distance += difference * difference;
+        }
+        distance = sqrt(distance);
+
+        return distance;
+    }
+
+    template<typename T>
+    T jaccard_distance(cv::Mat_<T> &point_a, cv::Mat_<T> &point_b, const int &_dim_num)
+    {
+        const T * point_a_ptr = reinterpret_cast<T *>(point_a.data);
+        const T * point_b_ptr = reinterpret_cast<T *>(point_b.data);
+
+        T minsum = 0, maxsum = 0;
+        for (size_t dim = 0; dim < _dim_num; ++dim)
+        {
+            minsum += std::min({*(point_a_ptr++), *(point_b_ptr++)});
+            maxsum += std::max({*(point_a_ptr++), *(point_b_ptr++)});
+        }
+        T dist = 1 - (minsum/maxsum);
+        return dist;
+    }
 } // namespace ModelFitting
